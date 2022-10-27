852367a308a5192d9f8891fc70c5bf64c5f6c778
sgurugubelli <sgurugubelli@ea.com>
2022-10-27 19:25:09 +0530

    Initial template for login service

---
+++ Login/src/main/java/com/bms/login/models/ERole.java
@@ -0,0 +1,7 @@
+package com.bms.login.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR
+}
---
+++ Login/src/main/java/com/bms/login/models/User.java
@@ -0,0 +1,54 @@
+package com.bms.login.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "userName"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max=20)
+    private String username;
+
+    @NotBlank
+    @Size(max=50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max=120)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
